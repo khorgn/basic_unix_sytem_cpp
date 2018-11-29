8620543b9c9acc2e26bd4a3742eb6106246a6118
DaMianYang <DaMianYang@outlook.fr>
2018-11-29 14:13:23 +0100

    found how to do add gtests to cmake to automatically launch all tests, in fibonacci

--- fibonacci_template/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hugo/Programming/C++/fibonacci_template
-# Build directory: /home/hugo/Programming/C++/fibonacci_template/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
