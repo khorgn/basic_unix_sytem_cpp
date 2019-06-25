3a2a95bdd3bf1d5f3d6f6dea2c9b4f401de85282
DaMianYang <hugo.dupuyroudel@outlook.com>
2019-06-25 23:09:33 +0200

    Corrected a typename

--- Haskell/learnyouahaskell/to_place_-_zipper.hs
+++ Haskell/learnyouahaskell/to_place_-_zipper.hs
@@ -25,24 +25,24 @@ consModify f (ConsZ ls x rs) = ConsZ ls (f x) rs
 -- tree zipper
 data Tree a = Node (Tree a) (Tree a) | Leaf a deriving (Show)
 data TreeCtx a = Top | L (TreeCtx a) (Tree a) | R (Tree a) (TreeCtx a) deriving Show
-type TreeLoc a = (Tree a, TreeCtx a)
+type TreeZ a = (Tree a, TreeCtx a)
 
-treeGoLeft :: TreeLoc a -> TreeLoc a
+treeGoLeft :: TreeZ a -> TreeZ a
 treeGoLeft (Node l r, c) = (l, L c r)
 
-treeGoRight :: TreeLoc a  -> TreeLoc a
+treeGoRight :: TreeZ a  -> TreeZ a
 treeGoRight (Node l r, c) = (r, R l c)
 
-treeGoUp :: TreeLoc a -> TreeLoc a
+treeGoUp :: TreeZ a -> TreeZ a
 treeGoUp (t, L c r) = (Node t r, c)
 treeGoUp (t, R l c) = (Node l t, c)
 
-treeSetTop :: Tree a -> TreeLoc a
+treeSetTop :: Tree a -> TreeZ a
 treeSetTop t = (t, Top)
 
-treeGoUpmost :: TreeLoc a -> TreeLoc a
+treeGoUpmost :: TreeZ a -> TreeZ a
 treeGoUpmost l@(t, Top) = l
 treeGoUpmost l = treeGoUpmost (treeGoUp l)
 
-treeModify :: (Tree a -> Tree a) -> TreeLoc a -> TreeLoc a
+treeModify :: (Tree a -> Tree a) -> TreeZ a -> TreeZ a
 treeModify f (t, c) = (f t, c)
