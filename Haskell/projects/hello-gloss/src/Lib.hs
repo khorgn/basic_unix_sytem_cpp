2c02f887fd1bdecce980d23f27931bc19348c6e7
DaMianYang <damianyang@outlook.fr>
2020-02-13 13:29:55 +0100

    Implemented a first version of thickline

--- Haskell/projects/hello-gloss/src/Lib.hs
+++ Haskell/projects/hello-gloss/src/Lib.hs
@@ -10,7 +10,10 @@ disp :: Picture -> IO ()
 disp = display (InWindow "Thick line" (300, 300) (10, 10)) white
 
 thickLine :: Path -> Float -> Picture
-thickLine = undefined
+thickLine (p1:p2:ps) = pictures [ thickLine1 p1 p2
+                                , thickLine (p2:ps)
+                                ]
+thickLine [] = blank
 
 p1 :: Point
 p1 = (0, 0)
@@ -18,12 +21,12 @@ p2 :: Point
 p2 = (100, 80)
 
 pic1 = translate (-50) 0 $ line [p1, p2]
-pic2 = translate 50 0 $ rec p1 p2 10
+pic2 = translate 50 0 $ thickLine1 p1 p2 10
 
 d = disp $ pictures [pic1, pic2]
 
-rec :: Point -> Point -> Float -> Picture
-rec p1 p2 t = translate midX midY $ rotate angle $ rectangleSolid width height
+thickLine1 :: Point -> Point -> Float -> Picture
+thickLine1 p1 p2 t = translate midX midY $ rotate angle $ rectangleSolid width height
   where (height, radian) = distanceRadian p1 p2
         width = t
         angle = radianToDegree radian
